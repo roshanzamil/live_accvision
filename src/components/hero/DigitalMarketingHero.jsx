019632a00df4f6e92d1c09b1c68bfafe95149e3e
roshan mef <roshan.mef@gmail.com>
2025-12-25 21:35:42 +0000

    You know how the video function works right? I want the description to b

--- src/components/hero/DigitalMarketingHero.jsx
+++ src/components/hero/DigitalMarketingHero.jsx
@@ -61,12 +61,14 @@ const DigitalMarketingHero = () => {
       videoClose.style.display = "block";
       heroArea.current.classList.add("start-video");
       headerArea.classList.add("bg-white");
+      heroTextAnim.current.style.color = "#145274";
     } else {
       wrapper.current.style.zIndex = "999";
       video.style.display = "block";
       videoClose.style.display = "none";
       heroArea.current.classList.remove("start-video");
       headerArea.classList.remove("bg-white");
+      heroTextAnim.current.style.color = "";
     }
   };
   return (
