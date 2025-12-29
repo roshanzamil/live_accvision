6845266ec9b7bb8e9319316504d0c29aae74e714
roshan mef <roshan.mef@gmail.com>
2025-12-29 04:34:22 +0000

    Failed to compile ./src/components/footer/Footer2.jsx Error: Failed to r

--- src/components/common/layout/RootLayout.jsx
+++ src/components/common/layout/RootLayout.jsx
@@ -7,47 +7,23 @@ import CursorAnimation from "../CursorAnimation";
 import ScrollTop from "../ScrollTop";
 import Header1 from "@/components/header/Header1";
 import Footer1 from "@/components/footer/Footer1";
-import Header2 from "@/components/header/Header2";
-import Header3 from "@/components/header/Header3";
-import Header4 from "@/components/header/Header4";
-import Header5 from "@/components/header/Header5";
-import Footer2 from "@/components/footer/Footer2";
-import Footer3 from "@/components/footer/Footer3";
-import Footer4 from "@/components/footer/Footer4";
-import Footer5 from "@/components/footer/Footer5";
 
 const HeaderContent = ({ header, navData }) => {
   if (header == "header1") {
     return <Header1 navData={navData} />;
-  } else if (header == "header2") {
-    return <Header2 navData={navData} />;
-  } else if (header == "header3") {
-    return <Header3 />;
-  } else if (header == "header4") {
-    return <Header4 navData={navData} />;
-  } else if (header == "header5") {
-    return <Header5 />;
   } else if (header == "none") {
     return "";
   } else {
-    return <Header3 />;
+    return <Header1 navData={navData} />;
   }
 };
 const FooterContent = ({ footer }) => {
   if (footer == "footer1") {
     return <Footer1 />;
-  } else if (footer == "footer2") {
-    return <Footer2 />;
-  } else if (footer == "footer3") {
-    return <Footer3 />;
-  } else if (footer == "footer4") {
-    return <Footer4 />;
-  } else if (footer == "footer5") {
-    return <Footer5 />;
   } else if (footer == "none") {
     return "";
   } else {
-    return <Footer3 />;
+    return <Footer1 />;
   }
 };
 
